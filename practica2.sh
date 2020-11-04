168dc5eae143e39ce611e83bad42fed6868d71b3
marc7666 <64266158+marc7666@users.noreply.github.com>
2020-11-04 17:31:49 +0100

    Add files via upload

---
+++ practica2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /proc
+awk 'BEGIN{system("date")}' > /home/mem_lliure.lst
+for i in {60..1}; do
+	awk '/MemTotal/{memt=$2; print memt};/MemFree/{memf=$2; print memf; exit}' meminfo >> /home/mem_lliure.lst
+	awk '{print $1}{exit}' loadavg >> /home/mem_lliure.lst #imprimim la carrega del sistema de l'ultim minut
+	awk 'BEGIN{FS=","}/dev/&&/size/{tmp=$3;print tmp}' mounts | awk 'BEGIN{FS="="}NR==1{print $2}' >> /home/mem_lliure.lst #imprimeix la memoria utilitzada i la disponible del sistema de fitxers montat
+	sleep 1
+done
+awk 'BEGIN{system("date")}' >> /home/mem_lliure.lst
+exit 0
