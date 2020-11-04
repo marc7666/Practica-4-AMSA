6ed5d62aca33abec9ba83d2a908409d6b9ba0788
marc7666 <64266158+marc7666@users.noreply.github.com>
2020-11-04 18:13:15 +0100

    Add files via upload

---
+++ practica2.2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+init 5
+
+#Servidor
+sudo su - #Validació de root per evitar introduir tot el rato la constrassenya
+apt-get update #Actualitzem el repositori de Ubuntu
+apt-get install apache2 #Instalem el servidor web
+sudo systemctl status apache2 #Comprobació de l'estat del servidor web
