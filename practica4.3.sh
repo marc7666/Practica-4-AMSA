d1f44794a9b0afbf2e9c9286f258e790c007817b
marc7666 <64266158+marc7666@users.noreply.github.com>
2020-11-07 12:52:12 +0100

    Update practica4.3.sh

--- practica4.3.sh
+++ practica4.3.sh
@@ -6,4 +6,3 @@ init 5
 sudo apt-get update #Actualitzem el repositori de Ubuntu
 sudo apt-get install apache2 #Instalem el servidor web
 sudo systemctl status apache2 #Comprobació de l'estat del servidor web
-locate initab
