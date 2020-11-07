c47c3e9244c413a4f821b57d5d1b8fc4fd8009d3
marc7666 <64266158+marc7666@users.noreply.github.com>
2020-11-07 12:36:37 +0100

    Update practica4.3.sh

--- practica4.3.sh
+++ practica4.3.sh
@@ -3,8 +3,7 @@
 init 5
 
 #Servidor
-sudo su - #Validació de root per evitar introduir tot el rato la constrassenya
-apt-get update #Actualitzem el repositori de Ubuntu
-apt-get install apache2 #Instalem el servidor web
+sudo apt-get update #Actualitzem el repositori de Ubuntu
+sudo apt-get install apache2 #Instalem el servidor web
 sudo systemctl status apache2 #Comprobació de l'estat del servidor web
 locate initab
